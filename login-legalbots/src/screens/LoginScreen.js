dfacf3e57ae940e669e0b8aa2b13eafb794f0456
rafaelribeiro96 <rafaelfelipe.r@hotmail.com>
2023-04-05 18:43:59 -0300

    feat: Adiciona inputs para login e senha com validação de e-mail e regras de validação de senha. Aplica estilos CSS iniciais.

--- login-legalbots/src/screens/LoginScreen.js
+++ login-legalbots/src/screens/LoginScreen.js
@@ -1,35 +1,114 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [buttonEnabled, setButtonEnabled] = useState(false);
+
+  // Verifica se o email e a senha são válidos para habilitar o botão de login
+  useEffect(() => {
+    const isValidEmail = /\S+@\S+\.\S+/.test(email);
+    const isValidPassword = password.length >= 8 && /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    setButtonEnabled(isValidEmail && isValidPassword);
+  }, [email, password]);
 
   const handleLogin = () => {
+    // Validando o email
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailError('Por favor, insira um email válido');
+      return;
+    } else {
+      setEmailError('');
+    }
+
+    // Validando a senha
+    if (password.length < 8 || !/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      setPasswordError('A senha deve conter pelo menos 8 caracteres e um caractere especial');
+      return;
+    } else {
+      setPasswordError('');
+    }
+
     // TODO: enviar solicitação ao servidor para verificar as credenciais de login
     // se as credenciais forem válidas, navegue para a tela inicial do aplicativo
     navigation.navigate('Home');
   };
 
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
       <TextInput
+        style={styles.input}
         placeholder="E-mail ou nome de usuário"
         value={email}
         onChangeText={setEmail}
       />
+      {emailError ? <Text style={styles.error}>{emailError}</Text> : null}
       <TextInput
+        style={styles.input}
         placeholder="Senha"
         secureTextEntry={true}
         value={password}
         onChangeText={setPassword}
       />
-      <TouchableOpacity onPress={handleLogin}>
-        <Text>Login</Text>
+      {passwordError ? <Text style={styles.error}>{passwordError}</Text> : null}
+      <TouchableOpacity
+        style={[styles.button, buttonEnabled ? styles.buttonEnabled : styles.buttonDisabled]}
+        onPress={handleLogin}
+        disabled={!buttonEnabled}
+      >
+        <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  error: {
+    color: 'red',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    opacity: 0.5, // definindo a opacidade inicial do botão de login
+  },
+  buttonEnabled: {
+    backgroundColor: '#007AFF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    opacity: 1, // definindo a opacidade habilitada do botão de login
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default LoginScreen;
