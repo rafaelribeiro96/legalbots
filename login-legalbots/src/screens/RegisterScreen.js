2f02e2190678c476bd613050db705d0f23b58cd8
rafaelribeiro96 <rafaelfelipe.r@hotmail.com>
2023-04-12 11:10:29 -0300

    refactor: refatorado código de cadastro de usuario

--- login-legalbots/src/screens/RegisterScreen.js
+++ login-legalbots/src/screens/RegisterScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text, TouchableOpacity } from 'react-native';
+import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import api from '../utils/api';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const RegisterScreen = ({ navigation }) => {
   const [name, setName] = useState('');
@@ -9,8 +10,12 @@ const RegisterScreen = ({ navigation }) => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
 
   const handleRegister = async () => {
+    const lowercaseEmail = email.toLowerCase();
+
     if (password !== confirmPassword) {
       setErrorMessage('As senhas não coincidem');
       return;
@@ -36,18 +41,18 @@ const RegisterScreen = ({ navigation }) => {
       return;
     }
 
-    if (email !== confirmEmail) {
+    if (lowercaseEmail !== confirmEmail.toLowerCase()) {
       setErrorMessage('Os endereços de email não coincidem');
       return;
     }
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
+    if (!emailRegex.test(lowercaseEmail)) {
       setErrorMessage('Endereço de email inválido');
       return;
     }
 
     try {
-      await api.post('/criar', { name, email, password });
+      await api.post('/criar', { name, email: lowercaseEmail, password });
       navigation.navigate('Login');
     } catch (error) {
       console.log(error);
@@ -55,53 +60,144 @@ const RegisterScreen = ({ navigation }) => {
     }
   };
 
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 24 }}>Cadastro de Usuário</Text>
-      {errorMessage ? <Text style={{ color: 'red', marginBottom: 12 }}>{errorMessage}</Text> : null}
+    <View style={styles.container}>
+
+      <Text style={styles.titlePage}>Cadastro de Usuário</Text>
+
+      {errorMessage ?
+        <Text style={styles.errorMessage}>
+          {errorMessage}
+        </Text>
+        : null}
+
       <TextInput
-        style={{ width: '80%', height: 48, borderColor: 'gray', borderWidth: 1, marginBottom: 12, paddingHorizontal: 8 }}
+        style={styles.textInput}
         onChangeText={setName}
         value={name}
         placeholder="Nome"
       />
+
       <TextInput
-        style={{ width: '80%', height: 48, borderColor: 'gray', borderWidth: 1, marginBottom: 12, paddingHorizontal: 8 }}
+        style={styles.textInput}
         onChangeText={setEmail}
         value={email}
         placeholder="E-mail"
         keyboardType="email-address"
       />
+
       <TextInput
-        style={{ width: '80%', height: 48, borderColor: 'gray', borderWidth: 1, marginBottom: 12, paddingHorizontal: 8 }}
+        style={styles.textInput}
         onChangeText={setConfirmEmail}
         value={confirmEmail}
         placeholder="Confirme seu e-mail"
         keyboardType="email-address"
       />
-      <TextInput
-        style={{ width: '80%', height: 48, borderColor: 'gray', borderWidth: 1, marginBottom: 12, paddingHorizontal: 8 }}
-        onChangeText={setPassword}
-        value={password}
-        placeholder="Senha"
-        secureTextEntry={true}
-      />
-      <TextInput
-        style={{ width: '80%', height: 48, borderColor: 'gray', borderWidth: 1, marginBottom: 12, paddingHorizontal: 8 }}
-        onChangeText={setConfirmPassword}
-        value={confirmPassword}
-        placeholder="Confirme sua senha"
-        secureTextEntry={true}
-      />
-      <TouchableOpacity onPress={handleRegister} style={{ backgroundColor: 'black', padding: 12, width: 250, margin: 10, borderRadius: 10 }}>
-        <Text style={{ color: 'white', fontSize: 18, textAlign: 'center', textTransform: 'uppercase' }}>Cadastrar</Text>
+
+      <View style={styles.passwordInput}>
+        <TextInput
+          placeholder="Insira sua senha"
+          secureTextEntry={!isPasswordVisible}
+          value={password}
+          onChangeText={setPassword}
+          style={styles.textPassword}
+        />
+        <TouchableOpacity
+          onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+        >
+          <MaterialIcons
+            name={isPasswordVisible ? 'visibility-off' : 'visibility'}
+            size={24}
+            color="#888"
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.passwordInput}>
+        <TextInput
+          placeholder="Confirme sua senha"
+          secureTextEntry={!isConfirmPasswordVisible}
+          onChangeText={setConfirmPassword}
+          value={confirmPassword}
+          style={styles.textPassword}
+        />
+        <TouchableOpacity
+          onPress={() => setIsConfirmPasswordVisible(!isConfirmPasswordVisible)}
+        >
+          <MaterialIcons
+            name={isConfirmPasswordVisible ? 'visibility-off' : 'visibility'}
+            size={24}
+            color="#888"
+          />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        onPress={handleRegister}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Login')} style={{ backgroundColor: 'black', padding: 12, width: 250, margin: 10, borderRadius: 10 }}>
-      <Text style={{ color: 'white', fontSize: 18, textAlign: 'center', textTransform: 'uppercase' }}>Já tenho uma conta</Text>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Login')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Já tenho uma conta</Text>
       </TouchableOpacity>
 
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titlePage: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 24
+  },
+  errorMessage: {
+    color: 'red',
+    marginBottom: 12
+  },
+  textInput: {
+    width: '80%',
+    height: 48,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8
+  },
+  passwordInput: {
+    width: '80%',
+    height: 48,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  textPassword: {
+    flex: 1
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 12,
+    width: 250,
+    margin: 10,
+    borderRadius: 10
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+    textTransform: 'uppercase'
+  }
+});
 
 export default RegisterScreen;
